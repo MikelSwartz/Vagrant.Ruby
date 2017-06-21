efcb856d0e588b823149b8131820427b0de65e0c
Mike <mikelswartz@gmail.com>
2017-06-21 12:06:27 -0400

    Added ruby to /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games

--- provision.sh
+++ provision.sh
@@ -8,7 +8,13 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 #\curl -sSL https://get.rvm.io | bash -s stable --ruby
 \curl -sSL https://raw.githubusercontent.com/wayneeseguin/rvm/stable/binscripts/rvm-installer | sudo bash -s stable
 
-rvm get stable
-yum install libyaml-devel -y
-rvm install ruby-2.4.0  --with-openssl-dir=$HOME/.rvm/usr --verify-downloads 1
 
+#echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
+
+/usr/local/rvm/bin/rvm get stable
+yum install libyaml-devel -y
+/usr/local/rvm/bin/rvm install ruby-2.4.0  #--with-openssl-dir=$HOME/.rvm/usr --verify-downloads 1
+
+cat >> /etc/bashrc <<EOF
+export PATH=$PATH:/usr/local/rvm/rubies/ruby-2.4.0/bin/
+EOF
