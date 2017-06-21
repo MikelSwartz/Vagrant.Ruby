6f22d11ffa88dcb8b80ba07ef792664b32131c50
Mike <mikelswartz@gmail.com>
2017-06-21 10:03:10 -0400

    Added ruby version 2.4.0

--- provision.sh
+++ provision.sh
@@ -8,3 +8,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 #\curl -sSL https://get.rvm.io | bash -s stable --ruby
 \curl -sSL https://raw.githubusercontent.com/wayneeseguin/rvm/stable/binscripts/rvm-installer | sudo bash -s stable
 
+rvm get stable
+yum install libyaml-devel -y
+rvm install ruby-2.4.0  --with-openssl-dir=$HOME/.rvm/usr --verify-downloads 1
+
